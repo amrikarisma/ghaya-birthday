49925cb160f9b7579d2e81c27beab7eccd8762ac
Amri Karisma <amrikarisma@live.com>
2023-06-08 08:44:54 +0700

    add modal

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
+const path = require('path')
 
-export default defineConfig({
-    assetsInclude: ['**/*.png', '**/*.mp3'],
-    base: './',
-})
+export default {
+    // assetsInclude: ['**/*.png', '**/*.mp3', '**/*.scss'],
+    // base: './',
+    root: path.resolve(__dirname, 'src'),
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
+}
