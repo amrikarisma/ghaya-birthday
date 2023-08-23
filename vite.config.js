6080abc0eb2f25b00a815451011933f4bf967c58
Amri Karisma <amrikarisma@live.com>
2023-08-23 22:34:25 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 const path = require('path')
 
 export default {
-    // assetsInclude: ['**/*.png', '**/*.mp3', '**/*.scss'],
-    // base: './',
     root: path.resolve(__dirname, 'src'),
     build: {
         outDir: path.resolve(__dirname, 'dist'),
